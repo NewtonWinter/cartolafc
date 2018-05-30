4ea2084be75a1872d05838daa8a854a126b48e1a
NewtonWinter <>
2018-05-30 13:58:35 -0300

    Conexão com o banco de dados da tela inicial

--- br.com.github.cartolafc/src/br/com/github/cartolafc/TelaInicialController.java
+++ br.com.github.cartolafc/src/br/com/github/cartolafc/TelaInicialController.java
@@ -5,6 +5,11 @@ package br.com.github.cartolafc;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -31,6 +37,8 @@ public class TelaInicialController implements Initializable {
     private Button btEntrar;
     @FXML
     private Button btNaoPossuiCadastro;
+    @FXML
+    private Label labelDadosIncorretos;
     
     @FXML
     private void irParaTelaDeCadastro(ActionEvent event) {
@@ -50,6 +58,42 @@ public class TelaInicialController implements Initializable {
             System.out.println("Senhor programador verifique o nome do arquivo FXML");
         }
     }
+    
+    @FXML
+    private void entrar(ActionEvent event1){
+        //labelDadosIncorretos.setVisible(true);
+        
+        //Fazendo conexao com o Banco de Dados
+        Connection connection = null;
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+                connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.canoas.ifrs.edu.br:1521:XE","ads_bd16","ads_bd16");       
+        }catch (ClassNotFoundException ex){
+            System.out.println("ops" + ex);
+        }catch (SQLException ex){
+            System.out.println("ops 2" + ex);
+        }
+        if(connection != null){
+            System.out.println("You have a new power!");
+        }else{
+            System.out.println("Noooooooooooo!");
+        }
+        
+        //Selecionando um dados da tabela
+        String selectSQL = "SELECT ? FROM usuario_java where nome = ";
+        
+        Statement st;
+        try {
+            st = connection.createStatement();
+            ResultSet rs = st.executeQuery(selectSQL);
+            while(rs.next()) {
+                Usuario p = new Usuario();
+                p.setNome(rs.getString(selectSQL));
+                p.setSenha(rs.getString(selectSQL));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     
     /**
      * Initializes the controller class.
