7be22f357c13f3eea4cf54197182a3d45d7ec222
NewtonWinter <>
2018-06-01 16:48:47 -0300

    criação da tela para cadastro dos filmes

---
+++ br.com.github.cartolafc/src/br/com/github/cartolafc/TelaCadastrarFilmesController.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.github.cartolafc;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class TelaCadastrarFilmesController implements Initializable {
+
+    @FXML
+    private TextField textoNomeFilme;
+    @FXML
+    private TextField textoDuracaoFilme;
+    @FXML
+    private TextField textoLancamentoFilme;
+    @FXML
+    private TextField textoPaisFilme;
+    @FXML
+    private TextField textoEstaNoAcervoFilmes;
+    @FXML
+    private CheckBox emprestadoSim;
+    @FXML
+    private CheckBox emprestadoNao;
+    @FXML
+    private TextArea textoSinopseFilme;
+    @FXML
+    private Button buttonMusica;
+    @FXML
+    private Button buttonPremiacoes;
+    @FXML
+    private Button buttonOndeFoiExibido;
+    @FXML
+    private Button buttonSerie;
+    @FXML
+    private Button buttonLivro;
+    @FXML
+    private Button buttonHistoriaReal;
+    @FXML
+    private Button buttonEstudio;
+    @FXML
+    private Button buttonDubladora;
+    @FXML
+    private Button buttonDiretor;
+    @FXML
+    private Button buttonAtores;
+    @FXML
+    private Button buttonAvaliacao;
+
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- br.com.github.cartolafc/src/br/com/github/cartolafc/TelaDeAberturaController.java
+++ br.com.github.cartolafc/src/br/com/github/cartolafc/TelaDeAberturaController.java
@@ -5,11 +5,17 @@
  */
 package br.com.github.cartolafc;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -24,7 +30,21 @@ public class TelaDeAberturaController implements Initializable {
     private Button buttonPesquisarFilme;
     @FXML
     private Button buttonListarFilmes;
-
+    
+    @FXML
+    private void irParaTelaCadastrarFilmes(ActionEvent event2) {
+        Parent root;
+        try {
+            //modo 2
+            Stage stage = ControleDeFilmes.stage;
+            root = FXMLLoader.load(getClass().getResource("TelaCadastrarFilmes.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (NullPointerException | IOException ex) {
+            System.out.println("Senhor programador verifique o nome do arquivo FXML");
+        }
+    }
+    
     /**
      * Initializes the controller class.
      */
