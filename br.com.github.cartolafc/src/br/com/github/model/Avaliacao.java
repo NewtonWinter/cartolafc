59c8f0e35deb2d8366db66ffd535380e7836c9a7
user <user@192.168.1.4>
2018-06-26 10:37:19 -0300

    Colocar os dados do filme no banco de dados parte 6

---
+++ br.com.github.cartolafc/src/br/com/github/model/Avaliacao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.github.model;
+
+/**
+ *
+ * @author user
+ */
+public class Avaliacao {
+    private int nota;
+    private String observacao;
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+}
