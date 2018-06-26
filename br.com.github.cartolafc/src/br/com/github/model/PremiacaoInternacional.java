05ee0667238528a1d30ab29f953ad4d1dd6269b0
user <user@192.168.1.4>
2018-06-26 10:38:08 -0300

    rer

---
+++ br.com.github.cartolafc/src/br/com/github/model/PremiacaoInternacional.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.github.model;
+
+/**
+ *
+ * @author user
+ */
+public class PremiacaoInternacional extends Premiacao{
+    
+    private String pais;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
---
+++ br.com.github.cartolafc/src/br/com/github/model/PremiacaoNacional.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.github.model;
+
+/**
+ *
+ * @author user
+ */
+public class PremiacaoNacional extends Premiacao{
+
+    private String cidade;
+    
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
